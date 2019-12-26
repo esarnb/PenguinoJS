062d30dfbe02302d9682da1bba86609964ca7c25
esarnb <esarnb@gmail.com>
2019-12-25 22:29:22 -0500

    help cmd code: getting cmd data, sorting and categorizing

--- commands/help.js
+++ commands/help.js
@@ -1,5 +1,17 @@
 exports.run = async (client, msg, args) => {
+    let categories = {};
+    for (cmd in client.commands) { 
+        if (!categories[cmd.help.type]) categories[cmd.help.type] = [];
+        categories[cmd.help.type].push(cmd.help);
+    }
     
+    let embed = new client.discord.MessageEmbed();
+    for (categ in categories) { 
+        categories[categ].sort((a, b) => a.name - b.name) 
+        embed.addField(categ, categories[categ].map(x => `${x.name}: ${x.desc}`).join("\n"))
+    }
+
+    msg.channel.send({embed: embed})
 }
 
 exports.help = {
