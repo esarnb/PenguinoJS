c16e36b5825bd86f875c67992413077048fa6452
esarnb <esarnb@gmail.com>
2019-12-25 23:19:28 -0500

    Edited ready msg, includes member, user, and bot counts

--- events/ready.js
+++ events/ready.js
@@ -5,5 +5,5 @@ module.exports = (client) => {
     let g = client.guilds.size; // g = guilds
     let t = client.users.size; // t = all users
     let u = client.users.filter(x => !x.bot).size; // u = users (not bots)
-    console.log( `Ready to serve ${g} servers, with ${u}/${t} users.`.green); 
+    console.log( `Ready to serve ${g} servers, with ${t} members: ${u} users and ${t-u} bots.`.green); 
 }
