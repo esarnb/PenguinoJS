5c55369e88e85ae47ff28ac5ab3790805dec0b4d
esarnb <esarnb@gmail.com>
2019-12-25 22:23:35 -0500

    help cmd structure

---
+++ commands/help.js
@@ -0,0 +1,13 @@
+exports.run = async (client, msg, args) => {
+    
+}
+
+exports.help = {
+    name: "help",
+    desc: "Shows all commands in categorical order",
+    type: "Info",
+    usage: "help <category>",
+    owner: false,
+    locked: false,
+    guild: false
+}
