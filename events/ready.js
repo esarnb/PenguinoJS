2e61494a1f7bb9f557f0e93bdaf5aafb9d246d24
esarnb <esarnb@gmail.com>
2022-04-12 15:22:44 +0000

    attach express to client and boot on ReadyEvent

--- events/ready.js
+++ events/ready.js
@@ -6,5 +6,6 @@ module.exports = async (client) => {
     let t = client.users.cache.size; // t = all users
     let u = client.users.cache.filter(x => !x.bot).size; // u = users (not bots)
     console.log( `${client.user.tag} ready to serve ${g} servers, with ${t} members: ${u} users and ${t-u} bots.`.green); 
-    require("../Express/express")(client); // Initialize Bot API
+    client.express = require("../Express/express"); // Initialize Bot API
+    client.express(client);
 }
